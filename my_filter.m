ce1cd4cbba17a3db1e4ce879c97318a812f76481
metrosxxmin <sxxmin.lee@gmail.com>
2019-10-29 11:20:42 +0900

    Modify the tabspace.

--- my_filter.m
+++ my_filter.m
@@ -19,13 +19,13 @@ if strcmp(type, 'avr')
 % applying weighted average filter
 elseif strcmp(type, 'weight')
 	mask = [1:pad_size+1 pad_size:-1:1]' * [1:pad_size+1 pad_size:-1:1];
-        s = sum(sum(mask));
-        for i = 1 : x
-                for j = 1 : y
-                        sum_1D = sum(double(pad_img(i : i+filter_size-1,j : j+filter_size-1)).*mask);
-                        filter_img(i, j) = sum(sum_1D)/s;
-                end
-        end
+    s = sum(sum(mask));
+    for i = 1 : x
+        for j = 1 : y
+            sum_1D = sum(double(pad_img(i : i+filter_size-1,j : j+filter_size-1)).*mask);
+            filter_img(i, j) = sum(sum_1D)/s;
+		end
+    end
 	
 % applying laplacian filter
 elseif strcmp(type, 'laplacian')
@@ -33,11 +33,11 @@ elseif strcmp(type, 'laplacian')
 % applying median filter
 elseif strcmp(type, 'median')
 	for i = 1 : x
-        	for j = 1 : y
+        for j = 1 : y
 			median_1D = median(double(pad_img(i : i+filter_size-1,j : j+filter_size-1)));
-            		filter_img(i, j) = median(median_1D); 
-        	end
-    	end
+            filter_img(i, j) = median(median_1D); 
+        end
+    end
 
 % applying sobel filter
 elseif strcmp(type, 'sobel')
@@ -45,15 +45,15 @@ elseif strcmp(type, 'sobel')
 	h_img = zeros(x, y);
 
 	v_mask = [1 : pad_size+1 pad_size : -1 : 1]' * [-pad_size : pad_size];
-    	h_mask = v_mask';
-    	for i = 1 : x
-        	for j = 1 : y
+    h_mask = v_mask';
+    for i = 1 : x
+        for j = 1 : y
 			temp = double(pad_img(i:i+filter_size-1,j:j+filter_size-1));
-            		v_img(i, j) = sum(sum(temp.*v_mask)); 
-            		h_img(i, j) = sum(sum(temp.*h_mask)); 
-            		filter_img(i, j) = v_img(i, j) + h_img(i, j);
-        	end
-    	end
+        	v_img(i, j) = sum(sum(temp.*v_mask)); 
+        	h_img(i, j) = sum(sum(temp.*h_mask)); 
+        	filter_img(i, j) = v_img(i, j) + h_img(i, j);
+        end
+    end
 % applying unsharped filter
 elseif strcmp(type, 'unsharp')
 	k = 0.5;
@@ -68,7 +68,6 @@ elseif strcmp(type, 'unsharp')
 		end
 	end
 end
-
 
 filter_img = uint8(filter_img);
 end
