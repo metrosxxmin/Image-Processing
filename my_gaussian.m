ede2e9685d3f0a6edd3e77eae315c974c5542389
metrosxxmin <sxxmin.lee@gmail.com>
2019-10-30 23:34:39 +0900

    Add a new M-file for gaussian filter.

---
+++ my_gaussian.m
@@ -0,0 +1,5 @@
+function filter_img = my_gaussian(img, filter_size, sigma)
+%  img: Image.     dimension (X x Y)
+%  fil_size: Filter maks(kernel)'s size.  type: (uint8)
+%  sigma: Sigma value of gaussian filter.  type: (double)
+
