ad5a7c8a1f18628fd3a2f09828da709117f0cafb
metrosxxmin <sxxmin.lee@gmail.com>
2019-11-02 00:13:46 +0900

    implement: image rotation using nearest method.

--- my_rotate.m
+++ my_rotate.m
@@ -8,7 +8,21 @@ s = sin(rad);
 [x, y] = size(img);
 f = [c s; -s c];
 
+row = (abs(c*x) + abs(s*y)); row_p = x / row;
+col = (abs(c*y) + abs(s*x)); col_p = y / col;
+re_img = zeros(row, col);
+
 if strcmp(interpolation, 'nearest')
+    for i = 1 : row
+       for j = 1 : col
+           v = f * (double([i; j]) - double([row/2; col/2]));
+           v = v + double([x/2; y/2]);
+           if v(1) < 1 || v(1) > x || v(2) < 1 || v(2) > y
+              continue; 
+           end           
+           re_img(i, j) = img(round(v(1)), round(v(2)));
+       end
+    end
 
 elseif strcmp(interpolation, 'bilinear')
 
