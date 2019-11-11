5eac8333d5f7cf34cc44d23c8f910dcc3115641a
metrosxxmin <52732734+metrosxxmin@users.noreply.github.com>
2019-11-11 17:52:57 +0900

    write: sequence's frame

--- my_decoding.m
+++ my_decoding.m
@@ -3,4 +3,7 @@ function img = my_decoding(zigzag,row,col)
 % zigzag : result of zigzag scanning
 % img    : GrayScale Image
 
-
+% 1. Construct 8x8 blocks using zigzag scanning value
+% 2. Multiply Quantization Table
+% 3. Apply Inverse DCT
+% 4. Add 128
